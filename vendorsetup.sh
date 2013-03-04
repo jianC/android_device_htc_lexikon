59982b78a5ca93d31669d816b5610c912daac0c0
e334 <e334cn@gmail.com>
2013-03-03 23:16:50 -0800

    For nightly builds

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo htc_lexikon-userdebug
-add_lunch_combo htc_lexikon-eng
+#add_lunch_combo htc_lexikon-userdebug
+#add_lunch_combo htc_lexikon-eng
